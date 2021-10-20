739904afc99f9f6f4392a4cfd29a293c3f307097
Istiak98 <istiakahmed104127@gmail.com>
2021-10-20 21:40:45 +0600

    13Update

--- src/Hooks/useFirebase.js
+++ src/Hooks/useFirebase.js
@@ -1,53 +1,115 @@
-import { getAuth, signInWithPopup, GoogleAuthProvider,signOut,onAuthStateChanged } from "firebase/auth";
-import { useEffect, useState } from "react";
+// import { getAuth, signInWithPopup, GoogleAuthProvider,signOut,onAuthStateChanged } from "firebase/auth";
+// import { useEffect, useState } from "react";
+// import initializeAuthentication from "../components/Shared/Login/Firebase/firebase.init";
+// initializeAuthentication();
+// const useFirebase=()=>{
+//     const [user,setUser]=useState({})
+//     // console.log(user)
+//   const [isLodaing,setIsLoading]=useState(true);
+
+
+
+//     const auth=getAuth();
+//     const signInUsingGoogle=()=>{
+//         setIsLoading(true);
+//    const googleProvider = new GoogleAuthProvider()
+//       signInWithPopup(auth,googleProvider)
+//       .then(result=>{
+//           setUser(result.user)
+//       })
+//       .finally(()=>setIsLoading(false));
+//     }
+// useEffect(()=>{
+//   const unsubcribed=  onAuthStateChanged(auth,user=>{
+//         if(user){
+//             setUser(user);
+
+//         }
+//         else{
+//             setUser({})
+//         }
+//         setIsLoading(false);
+//     });
+//     return()=>unsubcribed;
+
+// },[])
+
+//     const logOut=()=>{
+//         setIsLoading(true);
+//         signOut(auth)
+//         .then(()=>{})
+//         .finally(()=>setIsLoading(false));
+//     }
+//     return{
+//         user,
+//         isLodaing,
+//         signInUsingGoogle,
+//         logOut
+//     }
+
+// }
+
+
+// export default useFirebase;
+
+
+
+
+
+
+
+import { getAuth, signInWithPopup, GoogleAuthProvider, signOut, onAuthStateChanged } from "firebase/auth";
+import { useState, useEffect } from 'react';
 import initializeAuthentication from "../components/Shared/Login/Firebase/firebase.init";
+
 initializeAuthentication();
-const useFirebase=()=>{
-    const [user,setUser]=useState({})
-    // console.log(user)
-  const [isLodaing,setIsLoading]=useState(true);
 
+const useFirebase = () => {
+    const [user, setUser] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
 
+    const auth = getAuth();
 
-    const auth=getAuth();
-    const signInUsingGoogle=()=>{
+    const signInUsingGoogle = () => {
         setIsLoading(true);
-   const googleProvider = new GoogleAuthProvider()
-      signInWithPopup(auth,googleProvider)
-      .then(result=>{
-          setUser(result.user)
-      })
-      .finally(()=>setIsLoading(false));
+        const googleProvider = new GoogleAuthProvider();
+
+        signInWithPopup(auth, googleProvider)
+            .then(result => {
+                setUser(result.user);
+            })
+            .finally(() => setIsLoading(false));
     }
-useEffect(()=>{
-  const unsubcribed=  onAuthStateChanged(auth,user=>{
-        if(user){
-            setUser(user);
 
-        }
-        else{
-            setUser({})
-        }
-        setIsLoading(false);
-    });
-    return()=>unsubcribed;
+    // observe user state change
+    useEffect(() => {
+        const unsubscribed = onAuthStateChanged(auth, user => {
+            if (user) {
+                setUser(user);
+            }
+            else {
+                setUser({})
+            }
+            setIsLoading(false);
+        });
+        return () => unsubscribed;
+    }, [])
 
-},[])
-
-    const logOut=()=>{
+    const logOut = () => {
         setIsLoading(true);
         signOut(auth)
-        .then(()=>{})
-        .finally(()=>setIsLoading(false));
+            .then(() => { })
+            .finally(() => setIsLoading(false));
     }
-    return{
+
+    return {
         user,
-        isLodaing,
+        isLoading,
         signInUsingGoogle,
         logOut
     }
-
 }
 
-
 export default useFirebase;
+
+
--- src/components/Shared/Login/PrivateRoute/PrivateRoute.js
+++ src/components/Shared/Login/PrivateRoute/PrivateRoute.js
@@ -1,53 +1,25 @@
-// import React from "react";
-// import { Redirect, Route } from "react-router";
-// import useAuth from "../../../../Hooks/useAuth";
-// import { Spinner } from "react-bootstrap";
-// const PrivateRoute = ({ children, ...rest }) => {
-//   const { user, isLoading } = useAuth();
-//   if (isLoading) {
-//    return <Spinner animation="border" variant="info" />;
-//   }
-//   return (
-//     <Route
-//       {...rest}
-//       render={({ location }) =>user.email ? children  : <Redirect
-//             to={{
-//               pathname: "/login",
-//               state: { from: location }
-//             }}
-//           ></Redirect>
-        
-//       }
-//     ></Route>
-//   );
-// };
-
-// export default PrivateRoute;
-import React from 'react';
-import { Spinner } from 'react-bootstrap';
-import { Redirect, Route } from 'react-router';
+import React from "react";
+import { Redirect, Route } from "react-router";
 import useAuth from "../../../../Hooks/useAuth";
-
+import { Spinner } from "react-bootstrap";
 const PrivateRoute = ({ children, ...rest }) => {
-    const { user, isLoading } = useAuth();
-    if (isLoading) {
-        return <Spinner animation="border" variant="danger" />
-    }
-    return (
-        <Route
-            {...rest}
-            render={({ location }) => user.email ? children : <Redirect
-                to={{
-                    pathname: "/login",
-                    state: { from: location }
-                }}
-            ></Redirect>
-
-            }
-        >
-
-        </Route>
-    );
+  const { user, isLoading } = useAuth();
+  if (isLoading) {
+   return <Spinner animation="border" variant="info" />;
+  }
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>user.email ? children  : <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: location }
+            }}
+          ></Redirect>
+        
+      }
+    ></Route>
+  );
 };
 
 export default PrivateRoute;
