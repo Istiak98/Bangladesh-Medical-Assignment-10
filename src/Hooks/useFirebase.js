74b0bcd0449ff01064fbcea2e7e7270abd0f1c34
Istiak98 <istiakahmed104127@gmail.com>
2021-10-20 21:47:38 +0600

    14Update

--- src/Hooks/useFirebase.js
+++ src/Hooks/useFirebase.js
@@ -1,115 +1,53 @@
-// import { getAuth, signInWithPopup, GoogleAuthProvider,signOut,onAuthStateChanged } from "firebase/auth";
-// import { useEffect, useState } from "react";
-// import initializeAuthentication from "../components/Shared/Login/Firebase/firebase.init";
-// initializeAuthentication();
-// const useFirebase=()=>{
-//     const [user,setUser]=useState({})
-//     // console.log(user)
-//   const [isLodaing,setIsLoading]=useState(true);
-
-
-
-//     const auth=getAuth();
-//     const signInUsingGoogle=()=>{
-//         setIsLoading(true);
-//    const googleProvider = new GoogleAuthProvider()
-//       signInWithPopup(auth,googleProvider)
-//       .then(result=>{
-//           setUser(result.user)
-//       })
-//       .finally(()=>setIsLoading(false));
-//     }
-// useEffect(()=>{
-//   const unsubcribed=  onAuthStateChanged(auth,user=>{
-//         if(user){
-//             setUser(user);
-
-//         }
-//         else{
-//             setUser({})
-//         }
-//         setIsLoading(false);
-//     });
-//     return()=>unsubcribed;
-
-// },[])
-
-//     const logOut=()=>{
-//         setIsLoading(true);
-//         signOut(auth)
-//         .then(()=>{})
-//         .finally(()=>setIsLoading(false));
-//     }
-//     return{
-//         user,
-//         isLodaing,
-//         signInUsingGoogle,
-//         logOut
-//     }
-
-// }
-
-
-// export default useFirebase;
-
-
-
-
-
-
-
-import { getAuth, signInWithPopup, GoogleAuthProvider, signOut, onAuthStateChanged } from "firebase/auth";
-import { useState, useEffect } from 'react';
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
 import initializeAuthentication from "../components/Shared/Login/Firebase/firebase.init";
+import { useState, useEffect } from "react";
 
 initializeAuthentication();
 
 const useFirebase = () => {
-    const [user, setUser] = useState({});
-    const [isLoading, setIsLoading] = useState(true);
+const [user, setUser] = useState({});
+const [isLoading, setIsLoading] = useState(true);
+const auth = getAuth();
+const signInUsingGoogle = () => {
+    setIsLoading(true)
+    const googleProvider = new GoogleAuthProvider();
 
-    const auth = getAuth();
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        setUser(result.user);
+      })
+      .finally(() => setIsLoading(false))
+  };
+useEffect(() => {
+    const unsubscribed = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser({});
+      }
+      setIsLoading(false)
+    });
+    return () => unsubscribed;
+  }, []);
 
-    const signInUsingGoogle = () => {
-        setIsLoading(true);
-        const googleProvider = new GoogleAuthProvider();
+const logOut = () => {
+    setIsLoading(true)
+    signOut(auth)
+      .then(() => {})
+      .finally(() => setIsLoading(false))
+  };
 
-        signInWithPopup(auth, googleProvider)
-            .then(result => {
-                setUser(result.user);
-            })
-            .finally(() => setIsLoading(false));
-    }
-
-    // observe user state change
-    useEffect(() => {
-        const unsubscribed = onAuthStateChanged(auth, user => {
-            if (user) {
-                setUser(user);
-            }
-            else {
-                setUser({})
-            }
-            setIsLoading(false);
-        });
-        return () => unsubscribed;
-    }, [])
-
-    const logOut = () => {
-        setIsLoading(true);
-        signOut(auth)
-            .then(() => { })
-            .finally(() => setIsLoading(false));
-    }
-
-    return {
-        user,
-        isLoading,
-        signInUsingGoogle,
-        logOut
-    }
-}
-
+  return {
+    user,
+    isLoading,
+    signInUsingGoogle,
+    logOut,
+  };
+};
 export default useFirebase;
-
-
